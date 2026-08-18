95dcc3736a273c90c38a16030035e123f0944fd6
avdelag1 <woorkify@gmail.com>
2026-08-17 20:02:44 -0500

    fix(map): route web to direct discovery renderer

--- lib/src/features/map/presentation/screens/platform_discovery_map_impl_web.dart
+++ lib/src/features/map/presentation/screens/platform_discovery_map_impl_web.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_swipes/src/features/map/presentation/screens/web_discovery_map_screen.dart';
+import 'package:flutter_swipes/src/features/map/presentation/screens/web_discovery_map_screen_v2.dart';
 
 Widget buildPlatformDiscoveryMap({
   required VoidCallback? onClose,
   required bool showCitiesOnOpen,
 }) {
-  return WebDiscoveryMapScreen(
+  return WebDiscoveryMapScreenV2(
     onClose: onClose,
     showCitiesOnOpen: showCitiesOnOpen,
   );
