004d74bbc37656f3c79c740b72e8a80cc778613e
avdelag1 <woorkify@gmail.com>
2026-08-17 20:04:59 -0500

    fix(map): route web to compiler-safe direct renderer

--- lib/src/features/map/presentation/screens/platform_discovery_map_impl_web.dart
+++ lib/src/features/map/presentation/screens/platform_discovery_map_impl_web.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_swipes/src/features/map/presentation/screens/web_discovery_map_screen_v2.dart';
+import 'package:flutter_swipes/src/features/map/presentation/screens/web_discovery_map_screen_v3.dart';
 
 Widget buildPlatformDiscoveryMap({
   required VoidCallback? onClose,
   required bool showCitiesOnOpen,
 }) {
-  return WebDiscoveryMapScreenV2(
+  return WebDiscoveryMapScreenV3(
     onClose: onClose,
     showCitiesOnOpen: showCitiesOnOpen,
   );
