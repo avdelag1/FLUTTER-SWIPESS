22a94710bb59c6b3ad687389cd94f265c80f1bd9
avdelag1 <woorkify@gmail.com>
2026-08-22 16:25:07 -0500

    Expose full contract template workflow

--- lib/src/features/legal/presentation/providers/contracts_provider.dart
+++ lib/src/features/legal/presentation/providers/contracts_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_swipes/src/features/auth/presentation/providers/auth_provider.dart';
 import 'package:flutter_swipes/src/features/legal/data/repositories/contract_repository.dart';
+import 'package:flutter_swipes/src/features/legal/domain/contract_templates.dart';
 import 'package:flutter_swipes/src/features/legal/domain/digital_contract.dart';
 
 class ContractsNotifier extends AsyncNotifier<List<DigitalContract>> {
@@ -21,6 +22,14 @@ class ContractsNotifier extends AsyncNotifier<List<DigitalContract>> {
     final created = await ref
         .read(contractRepositoryProvider)
         .createFromTemplate(template);
+    await refresh();
+    return created;
+  }
+
+  Future<DigitalContract> duplicate(DigitalContract contract) async {
+    final created = await ref
+        .read(contractRepositoryProvider)
+        .duplicate(contract);
     await refresh();
     return created;
   }
