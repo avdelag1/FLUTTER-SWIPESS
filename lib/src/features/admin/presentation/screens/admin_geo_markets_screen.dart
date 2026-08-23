f9e7a6f3934c3ca5d1c94227e7c88281b3b7f5ee
Alejandro Villarreal <alejandro@example.com>
2026-08-23 00:00:37 -0500

    style: dart format

--- lib/src/features/admin/presentation/screens/admin_geo_markets_screen.dart
+++ lib/src/features/admin/presentation/screens/admin_geo_markets_screen.dart
@@ -9,67 +9,231 @@ class AdminGeoMarketsScreen extends ConsumerStatefulWidget {
   const AdminGeoMarketsScreen({super.key});
 
   @override
-  ConsumerState<AdminGeoMarketsScreen> createState() => _AdminGeoMarketsScreenState();
+  ConsumerState<AdminGeoMarketsScreen> createState() =>
+      _AdminGeoMarketsScreenState();
 }
 
 class _AdminGeoMarketsScreenState extends ConsumerState<AdminGeoMarketsScreen> {
   PassportCity? _selectedCity;
-  
+
   final Map<String, Map<String, bool>> _marketConfig = {};
 
   final _allCategories = <_GeoCategory>[
     // MOBILITY & TRANSPORT
-    _GeoCategory('properties', 'Properties & Rentals', 'mobility', Icons.home_work_rounded),
+    _GeoCategory(
+      'properties',
+      'Properties & Rentals',
+      'mobility',
+      Icons.home_work_rounded,
+    ),
     _GeoCategory('yachts', 'Yachts & Boats', 'mobility', Icons.sailing_rounded),
-    _GeoCategory('exotic_cars', 'Exotic Cars', 'mobility', Icons.directions_car_rounded),
-    _GeoCategory('private_jets', 'Private Jets & Helis', 'mobility', Icons.flight_takeoff_rounded),
-    _GeoCategory('motorcycles', 'Motorcycles', 'mobility', Icons.two_wheeler_rounded),
-    _GeoCategory('bicycles', 'Bicycles & Scooters', 'mobility', Icons.pedal_bike_rounded),
-    _GeoCategory('atv_quads', 'ATVs & Quads', 'mobility', Icons.agriculture_rounded),
-    _GeoCategory('golf_carts', 'Golf Carts', 'mobility', Icons.electric_car_rounded),
-    _GeoCategory('chauffeurs', 'Chauffeurs & Limos', 'mobility', Icons.airport_shuttle_rounded),
+    _GeoCategory(
+      'exotic_cars',
+      'Exotic Cars',
+      'mobility',
+      Icons.directions_car_rounded,
+    ),
+    _GeoCategory(
+      'private_jets',
+      'Private Jets & Helis',
+      'mobility',
+      Icons.flight_takeoff_rounded,
+    ),
+    _GeoCategory(
+      'motorcycles',
+      'Motorcycles',
+      'mobility',
+      Icons.two_wheeler_rounded,
+    ),
+    _GeoCategory(
+      'bicycles',
+      'Bicycles & Scooters',
+      'mobility',
+      Icons.pedal_bike_rounded,
+    ),
+    _GeoCategory(
+      'atv_quads',
+      'ATVs & Quads',
+      'mobility',
+      Icons.agriculture_rounded,
+    ),
+    _GeoCategory(
+      'golf_carts',
+      'Golf Carts',
+      'mobility',
+      Icons.electric_car_rounded,
+    ),
+    _GeoCategory(
+      'chauffeurs',
+      'Chauffeurs & Limos',
+      'mobility',
+      Icons.airport_shuttle_rounded,
+    ),
 
     // WORKERS & PROS
-    _GeoCategory('cleaners', 'Cleaning Staff', 'workers', Icons.cleaning_services_rounded),
-    _GeoCategory('maintenance', 'Maintenance', 'workers', Icons.build_circle_rounded),
-    _GeoCategory('security', 'Security & Bodyguards', 'workers', Icons.security_rounded),
-    _GeoCategory('translators', 'Translators / Guides', 'workers', Icons.translate_rounded),
-    _GeoCategory('chefs', 'Private Chefs', 'workers', Icons.restaurant_menu_rounded),
-    _GeoCategory('bartenders', 'Bartenders & Mixologists', 'workers', Icons.local_bar_rounded),
-    _GeoCategory('nannies', 'Nannies & Babysitters', 'workers', Icons.child_care_rounded),
-    
+    _GeoCategory(
+      'cleaners',
+      'Cleaning Staff',
+      'workers',
+      Icons.cleaning_services_rounded,
+    ),
+    _GeoCategory(
+      'maintenance',
+      'Maintenance',
+      'workers',
+      Icons.build_circle_rounded,
+    ),
+    _GeoCategory(
+      'security',
+      'Security & Bodyguards',
+      'workers',
+      Icons.security_rounded,
+    ),
+    _GeoCategory(
+      'translators',
+      'Translators / Guides',
+      'workers',
+      Icons.translate_rounded,
+    ),
+    _GeoCategory(
+      'chefs',
+      'Private Chefs',
+      'workers',
+      Icons.restaurant_menu_rounded,
+    ),
+    _GeoCategory(
+      'bartenders',
+      'Bartenders & Mixologists',
+      'workers',
+      Icons.local_bar_rounded,
+    ),
+    _GeoCategory(
+      'nannies',
+      'Nannies & Babysitters',
+      'workers',
+      Icons.child_care_rounded,
+    ),
+
     // BEAUTY, HEALTH & STYLE
     _GeoCategory('massage', 'Massage Therapists', 'beauty', Icons.spa_rounded),
-    _GeoCategory('makeup_artists', 'Makeup Artists', 'beauty', Icons.face_retouching_natural_rounded),
-    _GeoCategory('hair_stylists', 'Hair Stylists & Colorists', 'beauty', Icons.content_cut_rounded),
-    _GeoCategory('personal_trainers', 'Personal Trainers', 'beauty', Icons.fitness_center_rounded),
-    _GeoCategory('personal_shoppers', 'Personal Shoppers & Stylists', 'beauty', Icons.checkroom_rounded),
+    _GeoCategory(
+      'makeup_artists',
+      'Makeup Artists',
+      'beauty',
+      Icons.face_retouching_natural_rounded,
+    ),
+    _GeoCategory(
+      'hair_stylists',
+      'Hair Stylists & Colorists',
+      'beauty',
+      Icons.content_cut_rounded,
+    ),
+    _GeoCategory(
+      'personal_trainers',
+      'Personal Trainers',
+      'beauty',
+      Icons.fitness_center_rounded,
+    ),
+    _GeoCategory(
+      'personal_shoppers',
+      'Personal Shoppers & Stylists',
+      'beauty',
+      Icons.checkroom_rounded,
+    ),
 
     // EXPERIENCES & OUTDOORS
-    _GeoCategory('surf_lessons', 'Surf Instructors', 'experiences', Icons.surfing_rounded),
-    _GeoCategory('scuba', 'Scuba & Freediving', 'experiences', Icons.scuba_diving_rounded),
-    _GeoCategory('fishing', 'Fishing Charters', 'experiences', Icons.phishing_rounded),
-    _GeoCategory('ski', 'Ski & Snowboard', 'experiences', Icons.downhill_skiing_rounded),
-    _GeoCategory('desert_safari', 'Desert Safari & Camels', 'experiences', Icons.wb_sunny_rounded),
-    _GeoCategory('volcano_trekking', 'Volcano / Trekking Guides', 'experiences', Icons.terrain_rounded),
-    _GeoCategory('food_tours', 'Food & Culinary Tours', 'experiences', Icons.tapas_rounded),
-    _GeoCategory('wine_tastings', 'Sommeliers & Wine Tastings', 'experiences', Icons.wine_bar_rounded),
+    _GeoCategory(
+      'surf_lessons',
+      'Surf Instructors',
+      'experiences',
+      Icons.surfing_rounded,
+    ),
+    _GeoCategory(
+      'scuba',
+      'Scuba & Freediving',
+      'experiences',
+      Icons.scuba_diving_rounded,
+    ),
+    _GeoCategory(
+      'fishing',
+      'Fishing Charters',
+      'experiences',
+      Icons.phishing_rounded,
+    ),
+    _GeoCategory(
+      'ski',
+      'Ski & Snowboard',
+      'experiences',
+      Icons.downhill_skiing_rounded,
+    ),
+    _GeoCategory(
+      'desert_safari',
+      'Desert Safari & Camels',
+      'experiences',
+      Icons.wb_sunny_rounded,
+    ),
+    _GeoCategory(
+      'volcano_trekking',
+      'Volcano / Trekking Guides',
+      'experiences',
+      Icons.terrain_rounded,
+    ),
+    _GeoCategory(
+      'food_tours',
+      'Food & Culinary Tours',
+      'experiences',
+      Icons.tapas_rounded,
+    ),
+    _GeoCategory(
+      'wine_tastings',
+      'Sommeliers & Wine Tastings',
+      'experiences',
+      Icons.wine_bar_rounded,
+    ),
 
     // CULTURE & ENTERTAINMENT
     _GeoCategory('djs', 'DJs & VIP Hosts', 'culture', Icons.headphones_rounded),
-    _GeoCategory('photographers', 'Photographers / Paparazzi', 'culture', Icons.camera_alt_rounded),
-    _GeoCategory('spiritual', 'Spiritual Guides & Cacao', 'culture', Icons.self_improvement_rounded),
-    _GeoCategory('salsa', 'Salsa & Bachata Classes', 'culture', Icons.music_note_rounded),
+    _GeoCategory(
+      'photographers',
+      'Photographers / Paparazzi',
+      'culture',
+      Icons.camera_alt_rounded,
+    ),
+    _GeoCategory(
+      'spiritual',
+      'Spiritual Guides & Cacao',
+      'culture',
+      Icons.self_improvement_rounded,
+    ),
+    _GeoCategory(
+      'salsa',
+      'Salsa & Bachata Classes',
+      'culture',
+      Icons.music_note_rounded,
+    ),
     _GeoCategory('tango', 'Tango Classes', 'culture', Icons.music_note_rounded),
-    _GeoCategory('martial_arts', 'Martial Arts / Muay Thai', 'culture', Icons.sports_martial_arts_rounded),
-    _GeoCategory('kpop_dance', 'K-Pop / Dance Classes', 'culture', Icons.library_music_rounded),
+    _GeoCategory(
+      'martial_arts',
+      'Martial Arts / Muay Thai',
+      'culture',
+      Icons.sports_martial_arts_rounded,
+    ),
+    _GeoCategory(
+      'kpop_dance',
+      'K-Pop / Dance Classes',
+      'culture',
+      Icons.library_music_rounded,
+    ),
   ];
 
   @override
   void initState() {
     super.initState();
-    _selectedCity = PassportCities.all.firstWhere((c) => c.name == 'Miami', orElse: () => PassportCities.all.first);
-    
+    _selectedCity = PassportCities.all.firstWhere(
+      (c) => c.name == 'Miami',
+      orElse: () => PassportCities.all.first,
+    );
+
     // Seed global defaults & hyper-local specialities
     for (final city in PassportCities.all) {
       _marketConfig[city.name] = {
@@ -85,19 +249,30 @@ class _AdminGeoMarketsScreenState extends ConsumerState<AdminGeoMarketsScreen> {
         'chauffeurs': true,
         'photographers': true,
       };
-      
+
       final n = city.name;
-      
+
       // Tropical / Beach Hubs
-      if (['Tulum', 'Bali', 'Mykonos', 'Ibiza', 'Cabo', 'Punta Cana', 'Cancún', 'Miami', 'Sydney', 'Rio'].contains(n)) {
+      if ([
+        'Tulum',
+        'Bali',
+        'Mykonos',
+        'Ibiza',
+        'Cabo',
+        'Punta Cana',
+        'Cancún',
+        'Miami',
+        'Sydney',
+        'Rio',
+      ].contains(n)) {
         _marketConfig[n]!['yachts'] = true;
       }
-      
+
       if (['Tulum', 'Bali', 'Punta Cana', 'Cancún'].contains(n)) {
         _marketConfig[n]!['golf_carts'] = true;
         _marketConfig[n]!['scuba'] = true;
       }
-      
+
       if (['Tulum', 'Bali'].contains(n)) {
         _marketConfig[n]!['spiritual'] = true;
         _marketConfig[n]!['bicycles'] = true;
@@ -113,13 +288,29 @@ class _AdminGeoMarketsScreenState extends ConsumerState<AdminGeoMarketsScreen> {
       }
 
       // Mega-Luxury / VIP Nightlife
-      if (['Dubai', 'Monaco', 'Las Vegas', 'Miami', 'LA', 'London', 'Paris', 'New York'].contains(n)) {
+      if ([
+        'Dubai',
+        'Monaco',
+        'Las Vegas',
+        'Miami',
+        'LA',
+        'London',
+        'Paris',
+        'New York',
+      ].contains(n)) {
         _marketConfig[n]!['exotic_cars'] = true;
         _marketConfig[n]!['private_jets'] = true;
         _marketConfig[n]!['security'] = true;
       }
-      
-      if (['Ibiza', 'Mykonos', 'Las Vegas', 'Miami', 'Dubai', 'Tulum'].contains(n)) {
+
+      if ([
+        'Ibiza',
+        'Mykonos',
+        'Las Vegas',
+        'Miami',
+        'Dubai',
+        'Tulum',
+      ].contains(n)) {
         _marketConfig[n]!['djs'] = true;
       }
 
@@ -133,7 +324,15 @@ class _AdminGeoMarketsScreenState extends ConsumerState<AdminGeoMarketsScreen> {
       }
 
       // Culture, Food & Fashion capitals
-      if (['Paris', 'Rome', 'Barcelona', 'New York', 'London', 'Tokyo', 'Seoul'].contains(n)) {
+      if ([
+        'Paris',
+        'Rome',
+        'Barcelona',
+        'New York',
+        'London',
+        'Tokyo',
+        'Seoul',
+      ].contains(n)) {
         _marketConfig[n]!['translators'] = true;
         _marketConfig[n]!['food_tours'] = true;
         _marketConfig[n]!['personal_shoppers'] = true;
@@ -153,12 +352,12 @@ class _AdminGeoMarketsScreenState extends ConsumerState<AdminGeoMarketsScreen> {
         _marketConfig[n]!['tango'] = true;
       }
       if (['Tokyo', 'Seoul', 'Bangkok', 'Rio'].contains(n)) {
-        _marketConfig[n]!['martial_arts'] = true; 
+        _marketConfig[n]!['martial_arts'] = true;
       }
       if (n == 'Seoul' || n == 'LA') {
         _marketConfig[n]!['kpop_dance'] = true;
       }
-      
+
       // Fitness capitals
       if (['LA', 'Miami', 'Sydney', 'Rio'].contains(n)) {
         _marketConfig[n]!['personal_trainers'] = true;
@@ -208,7 +407,7 @@ class _AdminGeoMarketsScreenState extends ConsumerState<AdminGeoMarketsScreen> {
             ),
           ),
           const SizedBox(height: 24),
-          
+
           // City Selector
           SizedBox(
             height: 48,
@@ -235,7 +434,9 @@ class _AdminGeoMarketsScreenState extends ConsumerState<AdminGeoMarketsScreen> {
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(999),
                       border: Border.all(
-                        color: selected ? AppTheme.brandPrimary : ink.withAlpha(30),
+                        color: selected
+                            ? AppTheme.brandPrimary
+                            : ink.withAlpha(30),
                       ),
                     ),
                     backgroundColor: Colors.transparent,
@@ -244,7 +445,7 @@ class _AdminGeoMarketsScreenState extends ConsumerState<AdminGeoMarketsScreen> {
               },
             ),
           ),
-          
+
           const SizedBox(height: 16),
           if (_selectedCity != null)
             Padding(
@@ -282,9 +483,9 @@ class _AdminGeoMarketsScreenState extends ConsumerState<AdminGeoMarketsScreen> {
                 ],
               ),
             ),
-            
+
           const SizedBox(height: 24),
-          
+
           // Categories List
           Expanded(
             child: ListView(
@@ -302,12 +503,14 @@ class _AdminGeoMarketsScreenState extends ConsumerState<AdminGeoMarketsScreen> {
       ),
     );
   }
-  
+
   Widget _buildGroup(String title, List<String> groupIds, Color ink) {
     if (_selectedCity == null) return const SizedBox.shrink();
-    final categories = _allCategories.where((c) => groupIds.contains(c.groupId)).toList();
+    final categories = _allCategories
+        .where((c) => groupIds.contains(c.groupId))
+        .toList();
     if (categories.isEmpty) return const SizedBox.shrink();
-    
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -334,7 +537,12 @@ class _AdminGeoMarketsScreenState extends ConsumerState<AdminGeoMarketsScreen> {
               for (var i = 0; i < categories.length; i++) ...[
                 _buildCategoryTile(categories[i], ink),
                 if (i < categories.length - 1)
-                  Divider(height: 1, indent: 56, endIndent: 16, color: ink.withAlpha(15)),
+                  Divider(
+                    height: 1,
+                    indent: 56,
+                    endIndent: 16,
+                    color: ink.withAlpha(15),
+                  ),
               ],
             ],
           ),
@@ -342,15 +550,17 @@ class _AdminGeoMarketsScreenState extends ConsumerState<AdminGeoMarketsScreen> {
       ],
     );
   }
-  
+
   Widget _buildCategoryTile(_GeoCategory category, Color ink) {
     final isActive = _marketConfig[_selectedCity!.name]?[category.id] ?? false;
-    
+
     return ListTile(
       leading: Container(
         padding: const EdgeInsets.all(8),
         decoration: BoxDecoration(
-          color: isActive ? AppTheme.brandPrimary.withAlpha(20) : ink.withAlpha(10),
+          color: isActive
+              ? AppTheme.brandPrimary.withAlpha(20)
+              : ink.withAlpha(10),
           shape: BoxShape.circle,
         ),
         child: Icon(
