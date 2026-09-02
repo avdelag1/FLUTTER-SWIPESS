b218f51ad7e2c738db3df77c4c871eaed5acf672
avdelag1 <woorkify@gmail.com>
2026-09-02 11:31:25 -0500

    Add v2 recut stub

---
+++ lib/src/features/camera/data/video_recut_v2_stub.dart
@@ -0,0 +1,9 @@
+import 'package:image_picker/image_picker.dart';
+
+Future<XFile> recutVideoWindowV2({
+  required XFile source,
+  required double start,
+  required double end,
+  bool portraitCrop = false,
+  double cropX = 0.5,
+}) async => source;
