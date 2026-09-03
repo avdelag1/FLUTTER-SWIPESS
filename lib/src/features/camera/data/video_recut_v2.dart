328e1fbe24ad1c18aaaf97942ff089323d9d5cfa
avdelag1 <woorkify@gmail.com>
2026-09-03 00:51:31 -0500

    Use optimized web video export pipeline

--- lib/src/features/camera/data/video_recut_v2.dart
+++ lib/src/features/camera/data/video_recut_v2.dart
@@ -1,3 +1,3 @@
 export 'video_recut_v2_stub.dart'
-    if (dart.library.html) 'video_recut_v2_html.dart'
+    if (dart.library.html) 'video_recut_v3_html.dart'
     if (dart.library.io) 'video_recut_v2_io.dart';
